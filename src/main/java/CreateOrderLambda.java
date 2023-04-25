03bfb776688255026670c2e584ba50275b6e4ffc
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-04-24 20:51:18 -0400

    integrating with documentDb in createOrderLambda

--- src/main/java/CreateOrderLambda.java
+++ src/main/java/CreateOrderLambda.java
@@ -1,3 +1,8 @@
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -7,7 +12,16 @@ public class CreateOrderLambda {
     public APIGatewayProxyResponseEvent createOrder(APIGatewayProxyRequestEvent requestEvent) throws JsonProcessingException {
         final ObjectMapper objectMapper = new ObjectMapper();
         final Order order = objectMapper.readValue(requestEvent.getBody(), Order.class);
-        return new APIGatewayProxyResponseEvent().withBody(String.format("Order ID: " + order.getId()));
+
+        final DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
+        Table ordersTable = dynamoDB.getTable(System.getenv("ORDERS_TABLE"));
+
+        PutItemOutcome putItemOutcome = ordersTable.putItem(new Item()
+                .withPrimaryKey("id", order.id)
+                .withString("itemId", order.itemName)
+                .withInt("quantity", order.quantity));
+
+        return new APIGatewayProxyResponseEvent().withBody(String.format("Order ID: " + putItemOutcome.getItem().get("id")));
     }
 
 }
