63c990135480d2b4b37d122f3c6c5ff097523754
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-04-26 15:41:13 -0400

    Update template for the new s3 bucket reading.

--- HelloWorldFunction/src/main/java/helloworld/PatientCheckoutLambda.java
+++ HelloWorldFunction/src/main/java/helloworld/PatientCheckoutLambda.java
@@ -1,7 +1,39 @@
 package helloworld;
 
-public class PatientCheckoutLambda {
-    public void handler() {
 
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class PatientCheckoutLambda {
+    private final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public void handler(S3Event event) {
+        event.getRecords().forEach(record -> {
+            S3ObjectInputStream s3InputStream = s3.getObject(record
+                                    .getS3()
+                                    .getBucket()
+                                    .getName(),
+                            record
+                                    .getS3()
+                                    .getObject()
+                                    .getKey())
+                    .getObjectContent();
+
+            try {
+                List<PatientCheckoutEvent> patientCheckoutEvents = Arrays.asList(objectMapper.readValue(s3InputStream, PatientCheckoutEvent.class));
+                System.out.println(patientCheckoutEvents);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
     }
 }
