3ab7850678ceb867ee546b66012e723e099efd3d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-04-27 22:14:25 -0400

    Wire in and invoke the new sns topic for the events we parse

--- HelloWorldFunction/src/main/java/helloworld/PatientCheckoutLambda.java
+++ HelloWorldFunction/src/main/java/helloworld/PatientCheckoutLambda.java
@@ -5,6 +5,10 @@ import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.PublishResult;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -13,6 +17,7 @@ import java.util.List;
 
 public class PatientCheckoutLambda {
     private final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+    private final AmazonSNS sns = AmazonSNSClientBuilder.defaultClient();
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     public void handler(S3Event event) {
@@ -30,6 +35,13 @@ public class PatientCheckoutLambda {
             try {
                 List<PatientCheckoutEvent> patientCheckoutEvents = Arrays.asList(objectMapper.readValue(s3InputStream, PatientCheckoutEvent.class));
                 System.out.println(patientCheckoutEvents);
+                patientCheckoutEvents.forEach(patientCheckoutEvent -> {
+                    try {
+                        sns.publish(System.getenv("PATIENT_CHECKOUT_TOPIC"), objectMapper.writeValueAsString(patientCheckoutEvent));
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
